d4ded3a9b66db2ba99da2dcbe9230e39d15b3637
lorenzoSpec <john.erwin.lorenzo.2002@gmail.com>
2021-06-24 10:23:08 +0800

    showing top country with certain name

---
+++ js/index.js
@@ -0,0 +1,151 @@
+
+/* ===========================================
+
+    GLOBAL VARIABLES
+
+   ===========================================*/
+
+const CONFIRM = document.getElementById('confirm');
+const BD = document.getElementById('body');
+
+/*===========================================
+
+    ALGORITHMS  
+
+  ===========================================*/
+
+/* get the name given by user and display guesses */
+function nameInput(){
+  let inputNameVar = document.getElementById("input-name");
+  let name = inputNameVar.value;
+  nation(name);
+  removeSect();
+  inputNameVar.value = "";
+}
+
+/* fetch the api with given name */
+async function nation(nameGiven){
+  darkenScreen();
+
+  let nameToNation =  await fetch(`https://api.nationalize.io/?name=${nameGiven}`);
+  let res = await nameToNation.json();
+  let ctry = await res.country;
+
+  removeDarkScrn();
+  text(ctry);
+}
+
+/* to show what country it is */
+async function text(c){
+  let cCode = "";
+  if(c.length > 0){
+    cCode = c[0].country_id;
+  } 
+  console.log('cCode', cCode);
+
+  let cName = await fetch(`https://api.first.org/data/v1/countries?q=${cCode}`);
+  let cJson = await cName.json();
+  let cObj = await cJson.data[cCode];
+  let countryName = await cObj.country;  
+
+  console.log(countryName);
+  createGuess(countryName, cCode);
+}
+
+/* create a DOM for guess */
+function createGuess(country, cCode){
+  let flg = flag(cCode);
+
+  const CONT = document.createElement('div');
+  const IMG = document.createElement('img');
+  const PEL = document.createElement('p');
+  const DIV = document.createElement('div');
+  const BTNG = document.createElement('button');
+  const BTNN = document.createElement('button');
+
+  let PTXT = document.createTextNode(`I think it is ${country}`);
+  let GTXT = document.createTextNode('Great!');
+  let NTXT = document.createTextNode('No');
+
+  CONT.setAttribute('id', 'guess-div');
+  IMG.setAttribute('id', 'ctry-flag');
+  IMG.setAttribute('src', flg);
+  IMG.setAttribute('width', '300');
+  PEL.setAttribute('id', 'guess-p');
+  DIV.setAttribute('id', 'great-no');
+  BTNG.setAttribute('id', 'great-btn');
+  BTNN.setAttribute('id', 'no-btn');
+
+  CONT.appendChild(IMG);
+  CONT.appendChild(PEL);
+  DIV.appendChild(BTNG);
+  DIV.appendChild(BTNN);
+  CONT.appendChild(DIV);
+
+  PEL.appendChild(PTXT);
+  BTNG.appendChild(GTXT);
+  BTNN.appendChild(NTXT);
+
+  BD.appendChild(CONT);
+}
+
+/* function for dynamic showing of flag depends on name */
+function flag(cCode){
+  let f = cCode;
+  
+  let src = '';
+  if(f.length > 0){
+    src = `https://flagcdn.com/${f.toLocaleLowerCase()}.svg`;
+  }
+
+  return src;
+}
+
+/* to darken the screen */
+function darkenScreen(){
+  const darkScrn = document.createElement('div');
+  darkScrn.setAttribute('id', 'dark-screen');
+  BD.appendChild(darkScrn);
+}
+
+/* remove section from page */
+function removeSect(){
+  document.getElementById('input-name-cont').remove();
+}
+
+/* remove dark sreen div from page */
+function removeDarkScrn(){
+  document.getElementById('dark-screen').remove();
+}
+
+/*===========================================
+
+    SHORTCUT KEYS 
+
+  ===========================================*/
+
+/* shortcut for enter */
+function entr(e){
+  if(e.code === "Enter"){
+    nameInput();
+  }
+}
+
+/*===========================================
+
+   EVENT LISTENERS
+
+  ===========================================*/
+
+/* add events on the page */
+document.addEventListener('keyup', entr);
+CONFIRM.addEventListener('click', nameInput);
+
+
+/* clear events on page */
+function clearEvents(){
+  document.removeEventListener('keyup', entr);
+  CONFIRM.removeEventListener('click', nameInput);
+}
+
+window.addEventListener('beforeunload', clearEvents);
